0f7a03394bab9bc4703ab8af984bfa8f30443978
Fazlul Alam <fazlulalam619@gmail.com>
2024-07-31 21:11:30 +0600

    Jwt Related work

--- index.js
+++ index.js
@@ -3,11 +3,19 @@ const app = express();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
 // middleware
-app.use(cors({}));
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(cookieParser());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.crkhnnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,6 +28,33 @@ const client = new MongoClient(uri, {
   },
 });
 
+// middlewares
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next();
+};
+
+const verifyToken = (req, res, next) => {
+  const token = req?.cookies?.token;
+
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized access" });
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "unauthorized access" });
+    }
+    req.user = decoded;
+    next();
+  });
+};
+
+const cookeOption = {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production",
+  sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -31,6 +66,29 @@ async function run() {
       .db("bookstoreDB")
       .collection("authorsCollection");
 
+    // auth related api
+
+    app.post("/jwt", logger, async (req, res) => {
+      const user = req.body;
+
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.cookie("token", token, cookeOption).send({
+        status: true,
+      });
+    });
+
+    app.post("/logout", (req, res) => {
+      const user = req.body;
+
+      res
+        .clearCookie("token", { ...cookeOption, maxAge: 0 })
+        .send({ success: true });
+    });
+
+    // service related api
     // books post api
     app.post("/book-collection-post-api", async (req, res) => {
       const bookData = req.body;
@@ -51,7 +109,7 @@ async function run() {
       res.send(result);
     });
     // get books data for a specific user
-    app.get("/allBooks-get-api", async (req, res) => {
+    app.get("/allBooks-get-api", logger, verifyToken, async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
@@ -108,7 +166,7 @@ async function run() {
       res.send(result);
     });
     // get author data for a specific user
-    app.get("/allAuthors-get-api", async (req, res) => {
+    app.get("/allAuthors-get-api", logger, verifyToken, async (req, res) => {
       let query = {};
       if (req.query?.email) {
         query = { email: req.query.email };
